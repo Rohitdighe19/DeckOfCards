bed0b13207c68aeebfacf282ba84337cd648220e
rohitdighe19 <rohitdighe47@gmail.com>
2021-07-30 14:01:19 +0530

    Added to solution of use-case-1

---
+++ MainDeck.java
@@ -0,0 +1,12 @@
+public class MainDeck {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Deck Of Cards !!!");
+
+        //Object Creation
+        DeckOfCards deck=new DeckOfCards();
+
+        //calling method combineDeck()
+        deck.combineDeck();
+    }
+}
